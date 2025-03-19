54331fb24ac46d9c713ae298bfb05822001c751d
Armando Mora <armandomora14115@gmail.com>
2025-03-18 21:13:58 -0500

    -m ""

--- ejercicioUno.py
+++ ejercicioUno.py
@@ -6,43 +6,41 @@ num1 = int(input("Introduce el primer número: "))
 num2 = int(input("Introduce el segundo número: "))
 
 suma = num1 + num2
-print(f"\n{nombre}, el resultado de la suma es: {suma}\n")
+print(f"\nEl resultado de la suma es: {suma}\n")
 
 resta = num1 - num2
-print(f"{nombre}, el resultado de la resta es: {resta}\n")
+print(f"El resultado de la resta es: {resta}\n")
 
 multiplicacion = num1 * num2
-print(f"{nombre}, el resultado de la multiplicación es: {multiplicacion}\n")
+print(f"El resultado de la multiplicación es: {multiplicacion}\n")
 
 exponente = num1 ** num2
-print(f"{nombre}, el resultado de {num1} elevado a {num2} es: {exponente}\n")
+print(f"El resultado de {num1} elevado a {num2} es: {exponente}\n")
 
 cuadrado = num1 ** 2
-print(f"{nombre}, el cuadrado de {num1} es: {cuadrado}\n")
+print(f"El cuadrado de {num1} es: {cuadrado}\n")
 
 cuadrado = num2 ** 2
-print(f"{nombre}, el cuadrado de {num2} es: {cuadrado}\n")
+print(f"El cuadrado de {num2} es: {cuadrado}\n")
 
 if num2 != 0:
     division = num1 / num2
-    print(f"{nombre}, el resultado de la división es: {division}\n")
+    print(f"El resultado de la división es: {division}\n")
 else:
-    print(f"{nombre}, no se puede dividir por cero.\n")
+    print(f"No se puede dividir por cero.\n")
 
 if num2 != 0:
     modulo = num1 % num2
-    print(f"{nombre}, el resultado del módulo es: {modulo}\n")
+    print(f"El resultado del módulo es: {modulo}\n")
 else:
-    print(f"{nombre}, no se puede realizar el módulo por cero.\n")
+    print(f"No se puede realizar el módulo por cero.\n")
 
 
 if num2 != 0:
     division_entera = num1 // num2
-    print(f"{nombre}, el resultado de la división entera es: {division_entera}\n")
+    print(f"El resultado de la división entera es: {division_entera}\n")
 else:
-    print(f"{nombre}, no se puede realizar la división entera por cero.\n")
-
-print(f"¡Gracias por utilizar el programa de operaciones matemáticas, {nombre}!")
+    print(f"No se puede realizar la división entera por cero.\n")
 
 """
 Validacion de datos
@@ -66,4 +64,6 @@ print(f"El valor ingresado es: {texto}, y su tipo es: {type(texto)}\n")
 num1 = int(input("Introduce un primer número para comparar: "))
 num2 = int(input("Introduce un segundo número para comparar: "))
 comparacion = num1 == num2
-print(f"La comparación {num1} == {num2} es: {comparacion} (Booleano, True o False)\n")
+print(f"La comparación {num1} == {num2} es: {comparacion} \n")
+
+print(f"¡Gracias por utilizar el programa de operaciones matemáticas, {nombre}!")
